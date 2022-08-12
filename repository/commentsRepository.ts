0975e211b4ace86383b5948bff415c0678ec8bce
syd <syd@gnetsystem.com>
2022-08-12 11:32:21 +0900

    FIX : 유효성 검사 수정
    각 데이터 마다 유효성 체크 export 하도록 수정

--- repository/commentsRepository.ts
+++ repository/commentsRepository.ts
@@ -5,24 +5,24 @@ const TABLE_COMMENTS: string = process.env.TABLE_COMMENTS as string;
 
 
 async function writeCommentsdData(
-    _board_no : number,
+    _board_no: number,
     _contents: string,
     _author: string,
 ): Promise<number> {
     try {
-        let boardData : any[] =
-        await knex
-        .select('no')
-        .from(TABLE_BOARD)
-        .where('no', _board_no );
+        let boardData: any[] =
+            await knex
+                .select('no')
+                .from(TABLE_BOARD)
+                .where('no', _board_no);
 
 
-        if(boardData.length <= 0)
+        if (boardData.length <= 0)
             return -1;
 
         await knex(TABLE_COMMENTS)
             .insert({
-                board_no : _board_no,
+                board_no: _board_no,
                 contents: _contents,
                 author: _author,
                 createdAt: knex.fn.now()
@@ -36,6 +36,83 @@ async function writeCommentsdData(
     }
 
 }
+
+async function searchCommentsData(
+    _board_no: number,
+    _cursor: any,
+    _pageSize: number
+): Promise<string> {
+
+    try {
+        if (!await isExistBoard(_board_no))
+            return 'board_nothing';
+
+        let data: any = new Object();
+
+        //첫 페이지
+        if (isNaN(_cursor)) {
+            data = await knex
+                .select('no', 'contents', 'author', 'createdAt')
+                .from(TABLE_COMMENTS)
+                .where('board_no', _board_no)
+                .orderBy('no', 'desc')
+                .limit(_pageSize)
+
+            return data;
+        }
+
+        data = await knex
+            .select('no', 'contents', 'author', 'createdAt')
+            .from(TABLE_COMMENTS)
+            .where('no', '<', _cursor)
+            .andWhere('board_no', _board_no)
+            .orderBy('no', 'desc')
+            .limit(_pageSize)
+
+        return data;
+
+    } catch (err) {
+        console.log(err);
+        return 'error';
+    }
+}
+
+async function searchCommentsToalCount(
+    _board_no: number,
+): Promise<number> {
+    try {
+        if (!await isExistBoard(_board_no))
+            return -1;
+
+        let data: any =
+            await knex
+                .count('no as count')
+                .from(TABLE_COMMENTS)
+                .where('board_no', _board_no);
+
+        return data[0].count;
+
+    } catch (err) {
+        console.log(err);
+        return -100;
+    }
+}
+
+async function isExistBoard(_board_no: number): Promise<boolean> {
+    let boardData = await knex
+        .count('no as count')
+        .from(TABLE_BOARD)
+        .where('no', _board_no);
+
+    if (boardData[0].count <= 0)
+        return false;
+
+    return true;
+
+}
+
 export {
-    writeCommentsdData
+    writeCommentsdData,
+    searchCommentsData,
+    searchCommentsToalCount
 }
--- router/commentsRouter.ts
+++ router/commentsRouter.ts
@@ -6,9 +6,32 @@ import * as validation from '../lib/validation';
 
 ///댓글 작성
 router.post('/write',
-    validation.writeContentsValidation,
+    //validation.writeContentsValidation,
+    validation.boardNoBody.concat(
+        validation.contentsBody,
+        validation.authorBody
+    ),
     validation.validatorError,
     commentsControler.writeComments
+);
+
+///게시물 전체 댓글 수 조회
+router.get('/totalCount/:board_no' ,
+    //validation.boardNoValidation,
+    validation.boardNoParam,
+    validation.validatorError,
+    commentsControler.searchTotalCommentsCount
+);
+
+
+///댓글 조회 (페이징)
+router.get('/' ,
+    //validation.getCommentsDataValidation,
+    validation.pageSizeQuery.concat(
+        validation.boardNoQuery
+    ),
+    validation.validatorError,
+    commentsControler.searchComments
 );
 
 
